1bd791d363aedf04c68745e9277fddb6327cbee9
Cristian Camilo Isaza <sisaza92@gmail.com>
2017-06-22 15:43:55 -0500

    Base Proyect was created

---
+++ src/main/java/com/accenture/training/UditeamProyect1Application.java
@@ -0,0 +1,12 @@
+package com.accenture.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UditeamProyect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UditeamProyect1Application.class, args);
+	}
+}
